6b75c458fd40ed0ce066e2d7f41ecb33f4a8e896
Scotty Hagan <scott.hagan@aol.com>
2017-09-06 20:49:57 +0000

    refactored characterposition.js

--- characterposition.js
+++ characterposition.js
@@ -1,22 +1,17 @@
-function countLetters(str){
+function countLetters(str) {
   var output = {};
 
 // for every letter return the characters index in the array
 
   for (var i in str) {
     var propName = str[i];
-    if (/\w/.test(propName)){
-      if (output[propName]){
+    if (/\w/.test(propName)) {
+      if (!output[propName]) {
+        output[propName] = [];
+      }
       output[propName].push(i);
-      } else {
-        output[propName] = [i];
     }
-
-
-
-    }
-
   }
-return output;
+  return output;
 }
 console.log(countLetters("lighthouse in the house"));
